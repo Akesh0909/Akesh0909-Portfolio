24b6f30795e857c368be830e6798435deff8a278
Akesh Yadav <akeshrao0909a.r@gmail.com>
2024-01-20 14:53:40 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
+//import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
@@ -38,20 +38,20 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          onClick={() => {
-            updateExpanded(expand ? false : "expanded");
-          }}
-        >
+        // <Navbar.Brand href="/" className="d-flex">
+        //   <img src={logo} className="img-fluid logo" alt="brand" />
+        // </Navbar.Brand>
+        // <Navbar.Toggle
+        //   aria-controls="responsive-navbar-nav"
+        //   onClick={() => {
+        //     updateExpanded(expand ? false : "expanded");
+        //   }}
+        // >
         
-          <span></span>
-          <span></span>
-          <span></span>
-        </Navbar.Toggle>
+        //   <span></span>
+        //   <span></span>
+        //   <span></span>
+        // </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
